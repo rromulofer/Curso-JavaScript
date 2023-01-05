33803df505e10a0912560521f0fb701aaa2db393
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-05 20:37:50 -0300

    update

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log('O console funcionou corretamente!')
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel >= 50)
+    console.log(`Você ultrapassou o limite de velocidade`)
+
+console.log(`Dirija sempre utilizando cinto de segurança`)

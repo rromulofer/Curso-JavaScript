747280e6d228dcad4fd5a22bedb1b83e27a864a4
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-05 20:40:56 -0300

    update

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,8 +1,10 @@
-console.log('O console funcionou corretamente!')
-var vel = 80
+console.log('Radar de transito ativo!')
+var vel = 55
+
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
 
-if (vel >= 50)
-    console.log(`Você ultrapassou o limite de velocidade`)
+if (vel >= 50) {
+    console.log(`Você ultrapassou o limite de velocidade!`)
+}
 
 console.log(`Dirija sempre utilizando cinto de segurança`)

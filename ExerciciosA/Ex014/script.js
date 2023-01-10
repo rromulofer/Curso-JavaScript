30bcb56b828b1f9f4668e0312ac07de958b2aacf
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-10 02:37:28 -0300

    update

--- ExerciciosA/Ex014/script.js
+++ ExerciciosA/Ex014/script.js
@@ -7,18 +7,22 @@ function carregar() {
     //utilizando o var hora =  para realizar testes
     //para automatizar basta tirar o comentário do data.getHours()
     //e comentar a linha abaixo
-    var hora = 15
-
-    msg.innerHTML = `Agora são ${hora} horas`
+    var hora = 22
 
     if (hora >= 0 && hora < 12) {
         // Manha
+        msg.innerHTML = `Bom dia, agora são ${hora} horas`
         img.src = ('manha.png')
+        document.body.style.background = "#EEDD82"
     } else if (hora >= 12 && hora <= 18) {
         // Tarde
+        msg.innerHTML = `Boa tarde, agora são ${hora} horas`
         img.src = ('tarde.png')
+        document.body.style.background = "#D2691E"
     } else {
         // Noite
+        msg.innerHTML = `Boa noite, agora são ${hora} horas`
         img.src = ('noite.png')
+        document.body.style.background = "#104E8B"
     }
 }

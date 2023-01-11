de0fc8ba0a5b722a60193066cca2cf256ab65aa9
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-11 01:33:18 -0300

    Create ex015

---
+++ ExerciciosA/Ex015/script.js
@@ -0,0 +1,19 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById("res")
+    if(fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente.')
+    } else {
+        var fsex = document.getElementById('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ' '
+        if (fsex[0].checked){
+            genero = 'Homem'
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos` 
+    }
+}

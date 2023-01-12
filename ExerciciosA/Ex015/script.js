03bbd77197009e087176b292e74675e480f90fa7
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-12 02:46:52 -0300

    update

--- ExerciciosA/Ex015/script.js
+++ ExerciciosA/Ex015/script.js
@@ -3,17 +3,38 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById("res")
-    if(fano.value.length == 0 || Number(fano.value) > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente.')
     } else {
         var fsex = document.getElementById('radsex')
         var idade = ano - Number(fano.value)
         var genero = ' '
-        if (fsex[0].checked){
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
             genero = 'Homem'
-        } else if (fsex[1].checked){
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 29) {
+                //adulto
+            } else {
+                //idoso
+            }
+        } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 29) {
+                //adulto
+            } else {
+                //idoso
+            }
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos` 
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
 }
